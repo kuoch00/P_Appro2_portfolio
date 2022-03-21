40fed25fcc20cbbd23fddef9799aff066b665055
pietro <78962489+kuoch00@users.noreply.github.com>
2022-03-21 14:27:32 +0100

    mvc folders
    création des dossiers mvc et du header footer

---
+++ view/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+        <div>
+            <!--img header-->
+        </div>
+    </div>
+</header>
---
+++ view/view.php
@@ -0,0 +1,20 @@
+<!--
+    auteur : Elisa Kuoch
+    date de création : 21.03.2022
+    description : 
+
+
+-->
+<!--Style du template-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <link rel="stylesheet" href="../resources/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <!--link css override-->
+    <!--Link polices-->
+</head>
+    <?php
+    include("header.php");
+
+    ?>
