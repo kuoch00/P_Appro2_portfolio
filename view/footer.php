738700985cb2f70b74ad9d7ba5a6c42d0496af56
pietro <78962489+kuoch00@users.noreply.github.com>
2022-03-22 11:25:23 +0100

    footer

--- view/footer.php
+++ view/footer.php
@@ -1,39 +1,49 @@
 <footer>
     <div class="container">
+        
         <div class="footer-container ">
-            <h4>Sarah Souffleur</h4>
-            <!-- Grid container -->
-            <div class="">
-                <!-- Section: Social media -->
-                <section class="mb-2" >
-                    
-                    <!-- ArtStation -->
-                    <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-                        ><i class="fab fa-artstation"></i
-                    ></a>
+            <div class="footer-section">
+                <h4>Sarah Souffleur</h4>
+                <!-- Grid container -->
+                <div>
+                    <!-- Section: Social media -->
+                    <section class="mb-2" >
+                        
+                        <!-- ArtStation -->
+                        <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+                            ><i class="fab fa-artstation"></i
+                        ></a>
 
-                    <!-- Instagram -->
-                    <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-                        ><i class="fab fa-instagram"></i
-                    ></a>
+                        <!-- Instagram -->
+                        <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+                            ><i class="fab fa-instagram"></i
+                        ></a>
 
-                    <!-- Linkedin -->
-                    <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
-                        ><i class="fab fa-linkedin-in"></i
-                    ></a>
+                        <!-- Linkedin -->
+                        <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button"
+                            ><i class="fab fa-linkedin-in"></i
+                        ></a>
 
-                    <!-- Linkedin -->
-                    <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button">
-                        <i class="fa-solid fa-envelope"></i>
-                    </a>
+                        <!-- Linkedin -->
+                        <a class="btn btn-outline-light btn-floating m-2" href="#!" role="button">
+                            <i class="fa-solid fa-envelope"></i>
+                        </a>
 
-                </section>
-                
-                <!-- Section: Social media -->
+                    </section>
+                    <!-- Section: Social media -->
+                </div>
+                <!-- Grid container -->
+
+                <p></p>
             </div>
-            <!-- Grid container -->
-
-            <p></p>
+            <div class="footer-section">
+                <p></p>
+                <div class="text-center p-3">
+                    <p> © 2020 Copyright:</p>
+                </div>
+                <p></p>
+            </div>
         </div>
+        
     </div>
 </footer>
