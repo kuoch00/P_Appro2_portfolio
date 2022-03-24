18f6dda7943f3dff05b14f3184ca24b11365719a
pietro <78962489+kuoch00@users.noreply.github.com>
2022-03-24 09:35:49 +0100

    list and controller started

--- view/pages/projects/list.php
+++ view/pages/projects/list.php
@@ -7,4 +7,22 @@ description : page liste où tous les projets sont présentés
 
 
 
-<!-- liste tous les projets existants avec image de couverture -->
+<!-- liste tous les projets existants avec image de couverture et nom en dessous-->
+<div class="container">
+    <?php
+        foreach ($list_projects as $project){?>
+        <!-- affichage d'un projet  -->
+            <div class="container-item">
+                <!-- va a case gallery et on pourra aller ensuite dans catId  -->
+                <a href="?page=projects&catId=<?=$category["idCategory"]?>">
+
+                    <!-- affiche l'image  -->
+                    <img src="/resources/img/<?= $category["catImage"]?>" alt="image">
+
+                    <!-- affiche le nom du projet  -->
+                    <h3><?= $category["catName"] ?></h3>
+                </a>
+            </div>
+        <?php }
+    ?>
+</div>
