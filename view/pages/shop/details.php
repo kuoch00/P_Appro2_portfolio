7e115de161febbe1af88b9566e803c0122d45dc8
pietro <78962489+kuoch00@users.noreply.github.com>
2022-04-01 12:08:16 +0200

    affichage du shop (liste produits et détails)

--- view/pages/shop/details.php
+++ view/pages/shop/details.php
@@ -11,30 +11,40 @@ description : page où on consulte les détails d'un article
 <div class="container">
 <?php ?>
 <a href="?order=shop"><- retour</a><br>
-<img src="/resources/img-shop/<?=$products[$artId-1]["artImage"]?>" alt="image">
-<h4><?=$products[$artId-1]["artName"]?></h4>
-<h5>Description :</h5>
-<p><?= $products[$artId-1]["artDescription"]?></p>
-<h4><?=$products[$artId-1]["artPrice"]?> $</h4>
-<p>stock : <?=$products[$artId-1]["artStock"]?></p>
+<div class="row">
+    <div class="col-sm-3">
+        <img class="img-detail" src="resources/img-shop/<?=$products[$artId-1]["artImage"]?>" alt="image">
+    </div>
+    <div class="col-sm">
+        <h4><?=$products[$artId-1]["artName"]?></h4>
+        <h5>Description :</h5>
+        <p><?= $products[$artId-1]["artDescription"]?></p>
+        <h4><?=$products[$artId-1]["artPrice"]?> $</h4>
+        <p>stock : <?=$products[$artId-1]["artStock"]?></p>
 
-<form method="POST" action="?order=shop&artId=<?=$products[$artId-1]["idArticle"]?>&addedToCart=true ">
-    <!-- déjà dans panier ? -->
-    <?php
-        if(isset($_SESSION['cart'])){
-            if(isset($_SESSION['cart'])){
-                
-            }
-        }
-    ?>
+        <form method="POST" action="?order=shop&artId=<?=$products[$artId-1]["idArticle"]?>&addedToCart=true ">
+            <!-- déjà dans panier ? -->
+            <?php
+                if(isset($_SESSION['cart'])){
+                    if(isset($_SESSION['cart'])){
+                        
+                    }
+                }
+            ?>
 
-    <label for="quantity">Quantity</label>
+            <label for="quantity">Quantity</label>
 
-    <!-- si deja dans panier, indiquer la quantité -->
-    <input value="1" type="number" id="quantity" name="quantity" min="1" max="<?=$products[$artId-1]["artStock"]?>"> 
-    <!-- si déjà dans panier, message : cet article se trouve déjà dans votre panier ! -->
-    <button class="btn btn-primary formButtonSubmit" name="article" type="submit" value="<?=$products[$artId-1]["idArticle"]?>">Ajouter au panier</button>
-    
-</form>
+            <!-- si deja dans panier, indiquer la quantité -->
+            <input value="1" type="number" id="quantity" name="quantity" min="1" max="<?=$products[$artId-1]["artStock"]?>"> 
+            <!-- si déjà dans panier, message : cet article se trouve déjà dans votre panier ! -->
+            <button class="btn btn-primary formButtonSubmit" name="article" type="submit" value="<?=$products[$artId-1]["idArticle"]?>">Ajouter au panier</button>
+            
+        </form>
+    </div>
+
+</div>
+
+
+
 <?php ?>
 </div>
--- view/pages/shop/products.php
+++ view/pages/shop/products.php
@@ -4,20 +4,25 @@ auteur : Elisa Kuoch
 date de création : 22.03.2022
 description : page galerie où on consulte les articles
 -->
-
+<!-- https://getbootstrap.com/docs/4.4/components/card/  -->
 <!-- afficher nom et prix et photo -->
-<div class="container">
- <?php
- foreach ($products as $product){?>
- <div>
- <a href="?order=shop&artId=<?=$product["idArticle"]?>">
-    <img src="/resources/img-shop/<?=$product["artImage"]?>" alt="image">
-    <h4><?=$product["artName"]?></h4>
-    <p><?=$product["artPrice"]?> $</p>
- </a>
-    
- </div>
+<div class="container ">
+   <div class="container-flex row">
 
-<?php
- } ?>
+   <?php
+   
+   foreach ($products as $product){?>
+
+        
+            <div class="card col-sm-4" > 
+               <a href="?order=shop&artId=<?=$product["idArticle"]?>">
+                  <img class="img-card" src="resources/img-shop/<?=$product["artImage"]?>" alt="image">
+                  <h4><?=$product["artName"]?></h4>
+                  <p><?=$product["artPrice"]?> $</p> 
+               </a>
+            </div>
+         
+      <?php
+   } ?>
+ </div>
 </div>
