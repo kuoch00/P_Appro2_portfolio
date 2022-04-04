43fbc022cc5c61c45ec0cc2cb21e8866d4cd988c
pietro <78962489+kuoch00@users.noreply.github.com>
2022-04-04 11:19:26 +0200

    affichage galerie 2col

--- view/footer.php
+++ view/footer.php
@@ -70,7 +70,7 @@ description : footer de la page
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 <script>
-
+// source : https://stackoverflow.com/a/71326915
 const exampleModalEl = document.getElementById('exampleModal');
 
 exampleModalEl.addEventListener('show.bs.modal', function(event) {
--- view/pages/projects/gallery.php
+++ view/pages/projects/gallery.php
@@ -9,30 +9,59 @@ description : page galerie où on consulte les oeuvres
 titre du projet
 presente le projet brièvement  -->
 
-<!-- 2/3  colonnes verticales avec largeur fixée -->
+<!-- DONE : 2/3  colonnes verticales avec largeur fixée -->
 
-<!-- implementer modal view (js) -->
+<!-- DONE : implementer modal view (js) -->
 <div class="container">
-<a href="?page=projects&catId=<?=$listGallery[0]["idCategory"]?>">go back</a>
+<a class="btn btn-primary" href="?page=projects" role="button"><- go back</a>
+
 <?php
 
-//affiche la galerie
-    foreach($listGallery as $img){ 
-        // if($img["idImage"]%2==1){
-        //     echo $img["idImage"];
-        // }
-        ?>
-            <img class="gallery-width" src="resources/img/<?=$img["imaFilename"]?>" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal">
-
-            <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-xl">Extra large modal</button> -->
-
+  $i=0;
+        foreach($listGallery as $img){ 
+          if($i%2==0){
+            ?>
+            <!-- new row -->
+            <div class= "row">
+              <div class="col">
+                <img class="gallery-width thumb" src="resources/img/<?=$img["imaFilename"]?>" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              </div><!-- fin col -->
+            <?php
+          }
+          else{
+            ?>
+              <div class= col>
+                <img class="gallery-width thumb" src="resources/img/<?=$img["imaFilename"]?>" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              </div> <!-- fin col -->
+            </div> <!-- fin row -->
             
+            <?php
+          }
+          $i++;
+          ?>
+        
+        <?php } //fin foreach
 
-        <?php
-        } ?>
-        <!-- Modal -->
+
+// fin foreach 
+
+if($i%2==1){
+  ?>
+  <div class=col>
+    <p></p>
+  </div>
+  </div>
+  <?php
+      ?>
+
+      <?php
+}
+      ?> 
+      
+
 
 <!-- Modal -->
+<!-- source : https://stackoverflow.com/a/71326915 -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
