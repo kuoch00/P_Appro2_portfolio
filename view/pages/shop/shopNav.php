15722b4dc3ec8db28a830da752ebe8831f199843
pietro <78962489+kuoch00@users.noreply.github.com>
2022-04-06 15:43:38 +0200

    change text justify and create class

--- view/pages/shop/shopNav.php
+++ view/pages/shop/shopNav.php
@@ -3,7 +3,7 @@
         <div class="container" style="justify-content: flex-end;" >
             <div>
             <!-- a aligner a droite pour linstant a gauche -->
-                <a class="d-flex" href="?order=checkout" >
+                <a class="d-flex" href="?order=checkout">
                     
                         <i class="fa-solid fa-cart-shopping fa-2x"></i>
                         <p> Shopping Cart</p>
