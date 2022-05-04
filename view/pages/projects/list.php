34b7a6d71748fdec67344a3516ea8352c9e52569
pietro <78962489+kuoch00@users.noreply.github.com>
2022-05-04 14:43:31 +0200

    affichage 2 col des folders

--- view/pages/projects/list.php
+++ view/pages/projects/list.php
@@ -9,22 +9,25 @@ description : page liste où tous les projets sont présentés
 
 <!-- liste tous les projets existants avec image de couverture et nom en dessous-->
 <div class="container ">
-    <?php
-        foreach ($listProjects as $project){?>
-        <!-- affichage d'un projet  -->
-            <div class="container-item container-center">
-                <!-- va a case project et on pourra aller ensuite dans catId  -->
-                <a class="container-center" href="?page=projects&catId=<?=$project["idCategory"]?>">
+    <div class="row">
+        <?php 
+            foreach ($listProjects as $project){?>
+            <!-- affichage d'un projet  -->
+                <div class="container-item col-md-6">
+                    <!-- va a case project et on pourra aller ensuite dans catId  -->
+                    <a class="" href="?page=projects&catId=<?=$project["idCategory"]?>">
 
-                    <!-- affiche l'image  -->
-                    <div class="ratio ratio-21x9 mt-3">
-                        <img class="cat-cover"  src="resources/img/<?=$project["catImage"]?>" alt="image">
-                    </div>
+                        <!-- affiche l'image  -->
+                        <div class="ratio ratio-21x9 mt-3">
+                            <img class="cat-cover"  src="resources/img/<?=$project["catImage"]?>" alt="image">
+                        </div>
 
-                    <!-- affiche le nom du projet  -->
-                    <h3><?= $project["catName"] ?></h3>
-                </a>
-            </div>
-        <?php }
-    ?>
+                        <!-- affiche le nom du projet  -->
+                        <h3 class="text-center"><?= $project["catName"] ?></h3>
+                    </a>
+                </div>
+            <?php }
+        ?>
+    </div>
+    
 </div>
--- view/pages/projects/subfolders.php
+++ view/pages/projects/subfolders.php
@@ -18,49 +18,52 @@ liste les sujets du projet (bd)
 <!-- <div style="background: url('resources/img/treasuremap.jpg')  no-repeat center fixed ; background-size: cover;  ">
 </div> -->
 <div class="container ">
-<!-- style="background-image: url('resources/img/treasuremap.jpg');" -->
-    
-    <?php
+<?php
         if($_GET["catId"]>1){?>
             <a class="btn btn-primary" href="?page=projects" role="button"><i class="fa-solid fa-arrow-left"></i> go back</a>
         <?php
         } 
     ?>
+<!-- style="background-image: url('resources/img/treasuremap.jpg');" -->
+    <div class="row">
+    
     <h2><?=$projectInfo[0]["catName"]?></h2>
     <p><?=$projectInfo[0]["catDescription"]?></p>
 
     <?php
         //all
         ?>
-        <div class = "container-item container-center"> 
+        <div class = "container-item col-md-6"> 
             <?php $catId = $_GET['catId'] ?>
-            <a class="container-center" href="?page=projects&catId=<?=$catId?>&subcatId=all">
+            <a class=" " href="?page=projects&catId=<?=$catId?>&subcatId=all">
             <!-- id du projet + id du subfolder -->
             <!-- img derniere image ajoutée -->
                 <div class="ratio ratio-21x9">
                     <img class="cat-cover" src="resources/img/2.png" alt="image">
                 </div>
-                <h3>All</h3>
+                <h3 class="text-center">All</h3>
             </a> 
         </div>
         <?php
 
         //liste des sous cat
         foreach ($listSubcategories as $subcategory){?>
-            <div class = "container-item container-center">
+            <div class = "container-item col-md-6">
             <!-- id subcat?  -->
-                <a class="container-center" href="?page=projects&catId=<?= $subcategory["idCategory"]?>&subcatId=<?=$subcategory["idSubCategory"]?>">
+                <a class="" href="?page=projects&catId=<?= $subcategory["idCategory"]?>&subcatId=<?=$subcategory["idSubCategory"]?>">
                 <!-- id du projet + id du subfolder -->
                 <!-- img derniere image ajoutée -->
                     <div class="ratio ratio-21x9">
                         <img class="cat-cover" src="resources/img/2.png" alt="image">
                     </div>
                     
-                    <h3><?= $subcategory["subName"]?></h3>
+                    <h3 class="text-center"><?= $subcategory["subName"]?></h3>
                 </a> 
             </div>
 
     <?php } ?>
+    </div>
+    
     
     
     
