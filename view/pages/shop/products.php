dccaa21b6b4fe1df10eaa59a0f4d4142cb029454
pietro <78962489+kuoch00@users.noreply.github.com>
2022-05-06 11:15:21 +0200

    shop prduct list correction

--- view/pages/shop/products.php
+++ view/pages/shop/products.php
@@ -10,23 +10,24 @@ description : shop : page galerie où on consulte les articles
    <div class="row">
       <?php
       foreach ($products as $product){?> 
-         <div class="col col-sm-6 col-md-3 ">
+         <div class="col-12 col-sm-6 col-md-4 col-lg-3 ms-0 mt-3">
                <a href="?order=shop&artId=<?=$product["idArticle"]?>">
-                  <div class="card">
-                     <div class="artImg-container">
-                        <img class="img-card" src="resources/img-shop/<?=$product["artImage"]?>" alt="image">
+                  <div class="card rounded h-100">
+                     <div class="card-img-top;"style="width: 100%; height:70%; ">
+                        <img class="rounded-top" style="width: 100%; height:100%; object-fit:cover;"  src="resources/img-shop/<?=$product["artImage"]?>" alt="image">
                      </div>
-                     
-                  
-                     <h4><?=$product["artName"]?></h4>
-                     <div class="row">
-                        <div class="col-sm">
-                           <p><?= $product['artStock']>0 ? "stock : ". $product['artStock'] : "out of stock" ?></p>
-                        </div> 
-                        <div class="col-sm-5">
-                           <p style="text-align: right;"><?=$product["artPrice"]?> $</p>
+                     <div class="card-body">
+                        <h4 class="mt-1"><?=$product["artName"]?></h4>
+                        <div class="row">
+                           <div class="col-sm">
+                              <p><?= $product['artStock']>0 ? "stock : ". $product['artStock'] : "out of stock" ?></p>
+                           </div> 
+                           <div class="col-sm-5">
+                              <p style="text-align: right;"><?=$product["artPrice"]?> $</p>
+                           </div>
                         </div>
                      </div>
+                     
                   </div> 
                </a>
          </div>
